d325eae5137e30df8c9b62635332e5ed6dfa6999
Bronze-hammer <zihui_xu@126.com>
2020-06-10 18:46:15 +0800

    支付功能

--- src/main/java/com/victory2020/dao/OrderMapper.java
+++ src/main/java/com/victory2020/dao/OrderMapper.java
@@ -1,6 +1,7 @@
 package com.victory2020.dao;
 
 import com.victory2020.pojo.Order;
+import org.apache.ibatis.annotations.Param;
 
 public interface OrderMapper {
     int deleteByPrimaryKey(Integer id);
@@ -14,4 +15,6 @@ public interface OrderMapper {
     int updateByPrimaryKeySelective(Order record);
 
     int updateByPrimaryKey(Order record);
+
+    Order selectByUserIdAndOrderNo(@Param("userId") Integer userId, @Param("orderNo") Long orderNo);
 }
